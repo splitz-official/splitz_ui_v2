3b492b3341ff4f352c19103e72af630bad2de5c2
Charles Gutcho <cgutcho@berkeley.edu>
2024-01-21 11:43:33 -0800

    Test app.js

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import RootNavigator from "./ui/app/RootNavigator";
+import LandingScreen from "./ui/app/login_stack/screens/LandingScreen";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <RootNavigator />
+      <LandingScreen/>
     </NavigationContainer>
   );
 }
