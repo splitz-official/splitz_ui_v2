6cefe47075d0be21ff9c6ea3c3ad46be6750f957
Nikhil Aggarwal <nikhil0929@gmail.com>
2024-01-21 11:39:51 -0800

    Added OTP screen template

---
+++ ui/app/login_stack/screens/OTPScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, StyleSheet, Image, TouchableOpacity } from "react-native";
+
+function OTPScreen() {
+  return <View></View>;
+}
+
+export default OTPScreen;
