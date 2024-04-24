058a4f6da53beb2a382317f9e9c3cf907af70971
Raymond Dinh <raymondhdinh@gmail.com>
2024-04-23 23:07:41 -0700

    Added bill participants + sent navigation to upload/take photo and changed participants list item component

--- splitz_app/App_Stack/Split_Bill_stack/Components/Participants_list_item.js
+++ splitz_app/App_Stack/Split_Bill_stack/Components/Participants_list_item.js
@@ -1,21 +1,42 @@
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { scale } from 'react-native-size-matters'
+import { RFValue } from 'react-native-responsive-fontsize'
 
 const Participants_list_item = ({ name, username, onPress}) => {
   return (
     <TouchableOpacity style={styles.container} onPress={onPress}>
-        <Text>{name}</Text>
-        <Text>@{username}</Text>
+        <View style={styles.profilePicture}/>
+        <View style={styles.nameContainer}>
+        <Text style={styles.fullName}>{name}</Text>
+        <Text style={styles.usersName}>@{username}</Text>
+        </View>
     </TouchableOpacity>
   )
 }
 const styles = StyleSheet.create({
     container: {
         flexDirection: 'row',
-        borderWidth: 1,
-        padding: scale(10)
-    }
+        padding: scale(7),
+        marginTop: 2,
+    },
+    nameContainer: {
+      padding: scale(7),
+    },
+    profilePicture: {
+      borderWidth: 1,
+      borderRadius: 100,
+      height: scale(45),
+      width: scale(45),
+      marginRight: scale(10)
+    },
+    fullName: {
+      fontSize: RFValue(13),
+      marginBottom: scale(3)
+    },
+    usersName: {
+      color: "grey"
+    },
 })
 
 export default Participants_list_item
